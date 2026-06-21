e9b0b7bf320c5ffd7e45b95fb870156d479fc807
muyu <muyu0211@gmail.com>
2026-06-21 12:39:54 +0800

    消除硬编码

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wails from "@wailsio/runtime/plugins/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wails("./bindings")],
+  resolve: {
+    alias: {
+      "@templates": path.resolve(__dirname, "..", "templates"),
+    },
+  },
+  server: {
+    fs: {
+      allow: [path.resolve(__dirname, "..")],
+    },
+  },
 });
