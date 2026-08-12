dd22627df9bf9a3768724e587a37da4891b0e1cd
muyu <muyu0211@gmail.com>
2026-08-12 23:59:49 +0800

    新增配置文件

--- pkg/template/package_importer.go
+++ pkg/template/package_importer.go
@@ -22,14 +22,14 @@ const (
 
 var templateIDPattern = regexp.MustCompile(`^[A-Za-z0-9][A-Za-z0-9_-]{1,63}$`)
 
-// Package is the validated content of a local .zip template package.
+// Package is the validated content of a local .zip package.
 type Package struct {
 	Meta Meta
 	HTML string
 	CSS  string
 }
 
-// LoadPackageFromZip reads and validates a .zip template package.
+// LoadPackageFromZip reads and validates a .zip package.
 func LoadPackageFromZip(filePath string) (*Package, error) {
 	reader, err := zip.OpenReader(filePath)
 	if err != nil {
